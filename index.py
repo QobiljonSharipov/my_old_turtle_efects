9ed70f2cfcde46f81d451223fa085939c35dd039
sharipovqobiljon <qobiljonsharipov33@gmail.com>
2024-09-01 15:06:11 +0500

    my_old_turtle_project
    2022 year

---
+++ index.py
@@ -0,0 +1,1068 @@
+# 1-chiziq
+'''
+import turtle
+
+bob=turtle.Turtle()
+bob.forward(200 )
+
+'''
+'''
+import turtle
+b=turtle . Turtle ()
+b.forward(200)
+print('salom')
+b.forward(-100)
+print('qalay')
+b.forward(200)
+b.forward(-50)
+print("zo'rmi")
+b.forward(-100)
+b.forward(150)
+b.forward(-200)
+print('daxshat')
+
+
+import turtle
+b=turtle.Turtle()
+b.forward(300)
+b.forward(-10)
+print('salom')
+b.forward(200)
+b.forward(-100)
+print ('yaxshimisan')
+b.forward(10)
+b.forward(-200)
+print('phytonda')
+b.forward(20)
+b.forward(-50)
+print('grafika')
+b.forward(30)
+b.forward(-100)
+print('bilan')
+b.forward(20)
+b.forward(-150)
+print('ishlashni')
+b.forward(30)
+b.forward(-250)
+print("o'rgandim")
+b.forward(200)
+b.forward(-100)
+
+import turtle
+b=turtle.Turtle()
+b.forward(300)
+import turtle
+b=turtle.Turtle()
+b.forward(300//5)
+import turtle
+b=turtle.Turtle()
+b.forward(-300)
+import turtle
+b=turtle.Turtle()
+b.forward(-300//-5)
+import turtle
+b=turtle.Turtle()
+b.forward(100)
+'''
+
+'''
+from turtle import*
+#def fleur ():
+for i in range (200):
+        circle(190-i,90),speed(20)
+        left(90)
+        circle(190-i,90)
+        left(18)
+'''
+'''
+import turtle
+for i in range(4):
+        bob=turtle.Turtle()
+        bob.color('red')
+        bob.forward(300)
+        bob.color('green')
+        bob.left(90)
+        bob.forward(100)
+        bob.color('blue')
+        bob.left(90)
+        bob.forward(300)
+        bob.color('orange')
+        bob.left(90)
+        bob.forward(100)
+'''
+
+'''
+import turtle
+bob=turtle.Turtle()
+bob.speed(10)
+bob.color('red')
+bob.forward(300)
+bob.color('green')
+bob.left(90)
+bob.forward(100)
+bob.color('blue')
+bob.left(90)
+bob.forward(300)
+bob.color('orange')
+bob.left(90)
+bob.forward(100)    
+bob.speed(20)
+'''
+'''
+import turtle
+for i in range(6):
+        bob .color('black')
+        bob=turtle.Turtle()
+        bob.color('blue')
+        bob.speed(10)
+        bob.forward(100)
+        bob.left(45)
+        bob.forward(100)
+        bob.left(45)
+        bob.forward(100)
+        bob.left(45)
+        bob.forward(100)
+        bob.left(45)
+        bob.forward(100)
+        bob.left(45)
+        bob.forward(100)
+        bob.left(45)
+
+        bob.color('red')
+        bob.speed(10)
+        bob.forward(100)
+        bob.left(45)
+        bob.forward(100)
+        bob.left(45)
+        bob.forward(100)
+        bob.left(45)
+        bob.forward(100)
+        bob.left(45)
+        bob.forward(100)
+        bob.left(45)
+        bob.forward(100)
+        bob.left(45)
+'''
+
+''''
+from turtle import*
+def fleur():
+    for i in range(200):
+          circle(190-i,90),speed(60),color('blue')
+          left(90)
+          circle(190-i,90),speed(60),color('pink')
+          left(18)
+fleur()
+
+forward(-400)
+from turtle import*
+def flour():
+    for j in range(200):
+          circle(190-j,90),speed(60),
+          left(90)
+          circle(190-j,90),speed(60),
+          left(18)
+fleur()
+
+forward(800)
+from turtle import*
+def flour():
+    for j in range(200):
+          circle(190-j,90),speed(60),
+          left(90)
+          circle(190-j,90),speed(60),
+          left(18)
+
+fleur()
+
+
+right(90)
+forward(300)
+from turtle import*
+def fleur():
+    for i in range(190):
+          circle(190-i,90),speed(60),color('green')
+          left(90)
+          
+fleur()
+'''
+'''
+import turtle
+b=turtle .Turtle()
+b.speed(10)
+b.color('red')
+b. circle(360-200)    
+b.color('blue')
+b.circle(360-210)
+b.color('yellow')
+b.circle(360-220)
+b.color('green')
+b.circle(360-230)
+b.color('red')
+b.circle(360-240)
+b.color('blue')
+b.circle(360-250)
+b.color('yellow')
+b.circle(360-260)
+b.color('red')
+b.circle(360-270)
+b.color('blue')
+b.circle(360-280)
+b.color('green')
+b.circle(360-290)
+b.color('yellow')
+b.circle(360-300)
+b.color('green')
+b.circle(360-310)
+b.color('red')
+b.circle(360-320)
+b.color('blue')
+b.circle(360-330)
+b.color('green')
+b.circle(360-340)
+b.color('yellow')
+b.circle(360-350)
+b.color('red')
+b.circle(360-360)
+'''
+'''
+from turtle import*
+def fleur():
+    for i in range(190):
+          circle(190-i,90),speed(40),color('green')
+          left(90)
+          
+fleur()
+'''
+'''
+from turtle import*
+def pl():
+    for i in range(8):
+        left(45)
+        forward(150),color('blue'),speed(20)
+pl()
+'''
+'''
+from turtle import*
+color('green')
+begin_fill()
+while True:
+    forward(200)
+    left(170)
+    if abs (pos())<1:
+        break
+end_fill()
+done()
+'''
+# xato
+'''
+from turtle import*
+color('green')
+begin_fill()
+while True:
+    forward(200)
+    left(170)
+    if abs (pos())<1:
+        break
+end_fill()
+done()
+left(180)
+forward(300)
+from turtle import*
+def fleur():
+    for i in range(200):
+          circle(190-i,90),speed(60),color('blue')
+          left(90)
+          circle(190-i,90),speed(60),color('red')
+          left(18)
+fleur()
+left(130)
+'''
+'''
+import turtle
+from turtle import*
+
+turtle.title("spiral")
+speed(30)
+bgcolor("black")
+r,g,b=255,0,0
+
+for i in range(255*2):
+    colormode(255)
+    if i<255//3:
+        g+=3
+    elif i<255*2//3:
+        r-=3
+    elif i<255:
+        b+=3
+    elif i<255*4//3:
+        g-=3
+    elif i<255*5//3:
+        r+=3
+    else:
+        b-=3
+    fd(50+i)
+    rt(91)
+    pencolor(r,g,b)
+'''
+#paxta
+'''
+import turtle
+from turtle import*
+bgcolor("black")
+
+from turtle import*
+color('white')
+begin_fill()
+for i in range(3):
+       
+    circle(190)
+    left(90)
+end_fill()          
+done()
+'''
+#to'rtburchak
+'''
+import turtle
+from turtle import*
+bgcolor('black')
+color('blue')
+begin_fill()
+for i in range(4):
+    
+    forward(300)
+    left(90)
+end_fill()
+'''
+#ikki uchburchak
+'''
+import turtle
+from turtle import*
+bgcolor('black')
+color('yellow')
+begin_fill()
+for i in range(3):
+    forward(300)
+    left(150)
+end_fill()
+'''
+'''
+import turtle
+from turtle import*
+bgcolor('black')
+color('yellow')
+circle(160)
+
+color('blue')
+begin_fill()
+for i in range (3):
+    forward (300)
+    left(150)
+end_fill()
+'''
+
+# shar
+'''
+import turtle
+from turtle import*
+
+turtle.title("SHAR")
+bgcolor('black')
+color('red')
+begin_fill()
+circle(190)
+left(-45)
+forward(20)
+left(-120)
+forward(20)
+left(-120)
+forward(20)
+left(-140)
+end_fill()
+forward(300)
+'''
+
+
+# V
+'''
+import turtle
+from turtle import*
+bgcolor('black')
+color('cyan','blue')
+pensize(5)
+begin_fill()
+left(45)
+forward(300)
+right(-130)
+forward(90)
+left(50)
+forward(180)
+left(-90)
+forward(180)
+left(50)
+forward(90)
+left(130)
+forward(300)
+end_fill()
+'''
+'''
+import turtle
+from turtle import*
+bgcolor('black')
+color('white','yellow')
+begin_fill()
+
+for i in range(40):
+      circle(180-i,10),speed(20)
+end_fill()
+forward()
+'''
+
+'''
+import turtle as t
+import colorsys
+
+t.bgcolor('black')
+h=1
+t.tracer(200)
+t. pensize(1)
+def drawing(o, n):
+    t.circle(5+n,60)
+    t.right(a)
+    t.circle(5+n,60)
+t.goto(0,0)
+for i in range(600):
+    c =colorsys.hsv_to_rgb(h, 1, 0.8)
+    h+=0.005
+    t.pencolor(c)
+    draw(1/2, 0)
+    draw(180, i)
+t.done(c)    
+'''
+'''
+#gul qizil
+import turtle
+from turtle import*
+bgcolor('black')
+color('red')
+for i in range(250):
+    circle(180-i),speed(20)
+    left(90)
+'''        
+#quyosh
+'''
+
+import turtle
+from turtle import*
+bgcolor('black')
+color('yellow')
+for i in range(600):
+    forward(180-i),speed(20)
+    left(130)
+
+'''
+#yulduz
+
+'''
+import turtle
+from turtle import*
+bgcolor('black')
+color('yellow')
+for i in range(600):
+    forward(180-i),speed(20)
+    left(170)
+'''
+#uchburchak yulduz
+'''
+import turtle
+from turtle import*
+bgcolor('black')
+color('green')
+for i in range(350):
+    forward(180-i),speed(20)
+    left(120)
+'''
+#Virus
+'''
+import turtle
+from turtle import *
+bgcolor('black')
+color('green')
+for i in range(200):
+    circle(180-i,90),speed(20)
+    left(170)
+'''
+# bumeran
+'''
+import turtle
+from turtle import *
+bgcolor('black')
+color('green')
+for i in range(180):
+    circle(180-i,90),speed(20)
+    left(150)
+'''
+#gul 3
+'''
+import turtle
+from turtle import *
+bgcolor('black')
+color('yellow')
+for i in range(200):
+    circle(180-i,90),speed(20)
+    left(130)
+'''
+# teshik kulcha
+'''
+import turtle
+from turtle import *
+bgcolor('black')
+color('red')
+for i in range(150):
+    circle(180-i,90),speed(20)
+    left(25)
+'''
+#gul4
+'''
+import turtle
+from turtle import *
+bgcolor('black')
+color('blue')
+for i in range(150):
+    circle(180-i,90),speed(20),color('blue')
+    left(90)
+    circle(180-i,90)
+    left(50)
+'''
+#charxpalak
+'''
+import turtle
+from turtle import*
+bgcolor('black')
+for i in range (200):
+    forward(180-i),color('red'),speed(20)
+    left(90)
+    forward(180-i)
+    left(65)
+'''
+############
+##  o    ##
+############
+'''
+import turtle
+from turtle import *
+bgcolor('black')
+color('blue')
+pensize(50)
+circle(90)
+'''
+####################
+##   Flag    #######
+####################
+'''
+import turtle
+from turtle import*
+bgcolor('black')
+color('red','green')
+begin_fill()
+left(180)
+forward(300)
+left(90)
+forward(150)
+left(90)
+forward(600)
+left(90)
+forward(150)
+end_fill()
+color('red','white')
+begin_fill()
+
+left(90)
+pensize(10)
+forward(600)
+right(90)
+pensize(1)
+forward(150)
+right(90)
+pensize(10)
+forward(600)
+pensize(1)
+end_fill()
+color('red','blue')
+begin_fill()
+left(90)
+forward(150)
+left(90)
+forward(600)
+left(90)
+forward(150)
+left(90)
+pensize(10)
+forward(600)
+
+end_fill()
+'''
+'''
+import turtle
+from turtle import*
+bgcolor('black')
+color('yellow')
+for i in range(50):
+    circle(50),speed(20)
+    left(30)
+'''
+##################
+##      P       ##
+##################
+'''
+
+from turtle import*
+bgcolor('black')
+color('white')
+begin_fill()
+left(90)
+forward(100)
+right(90)
+forward(20)
+circle(70,70)
+circle(70,70)       
+circle(50,50)
+right(10)
+forward(50)
+left(90)
+forward(250)
+left(90)
+forward(25)
+left(90)
+forward(130)
+right(90)
+end_fill()
+color('black')
+begin_fill()
+forward(20)
+circle(50,50)
+circle(50,50)
+circle(50,50)
+circle(50,50)
+forward(4)
+left(70)
+forward(100)
+end_fill()
+'''
+###################
+##      T        ##
+####################
+'''
+from turtle import*
+bgcolor('black')
+color('blue')
+begin_fill()
+left(90)
+forward(210)
+right(90)
+forward(80)
+left(90)
+forward(30)
+left(90)
+forward(200)
+left(90)
+forward(30)
+left(90)
+forward(80)
+right(90)
+forward(210)
+left(90)
+forward(40)
+end_fill()
+'''
+
+#####################
+#colored flower######
+#####################
+'''
+import turtle as tur
+import colorsys as cs
+tur .setup(800,800)
+tur.speed(10)
+tur.tracer(10)
+tur.width(2)
+tur.bgcolor('black')
+for j in range(35):
+    for i in range(25):
+        tur.color(cs.hsv_to_rgb(i/15,j/25,1))
+        tur.right(90)
+        tur.circle(200-j*4,90)
+        tur.left(90)
+        tur.circle(200-j*4,90)
+        tur.right(180)
+        tur.circle(50,24)
+        tur.color(cs.hsv_to_rgb(i/15,j/25,1))
+        tur.right(90)
+        tur.circle(200-j*4,90)
+        tur.left(90)
+        tur.circle(200-j*4,90)
+        tur.right(180)
+        tur.circle(50,24)
+'''
+###############
+# star#########
+###############
+'''
+from turtle import*
+speed(20)
+fillcolor("purple")
+begin_fill()
+sides=5
+distance=100
+for _ in range(50*sides):
+    distance+=20
+    forward(distance)
+    right(2*360.0/sides+1)
+end_fill()
+'''
+#############
+##neon tunel#
+#############
+'''
+import turtle 
+t=turtle.Turtle()
+wn=turtle.Screen()
+wn.bgcolor('black' )
+t.speed(20)
+sides=6
+colors=["yellow","green","cyan","magenta","cyan","purple"]
+for i in range(360):
+    t.pencolor(colors[i%sides])
+    t.forward(i*3/sides+i)
+    t.left(360/sides+1)
+    t.width(i*sides/200)
+'''
+###################
+#colored ball######
+###################
+'''
+from turtle import*
+
+speed(20)
+bgcolor("black")
+import colorsys as t
+
+for i in range(100):
+    for j in range(25):
+         color(t.hsv_to_rgb(i/15,j/25,1))
+         circle(180)
+         left(18-i)
+         color(t.hsv_to_rgb(i/15,j/25,1))
+         circle(180-i)
+         left(18)
+'''
+################
+#colored flover#
+################
+'''
+from turtle import*
+import colorsys as t
+bgcolor("black")
+speed(20)
+for i in range(200):
+    m=i
+    for j in range (35):
+            color(t.hsv_to_rgb(i/15,j/25,1))
+            circle(190-m,90)
+            left(90)
+            color(t.hsv_to_rgb(i/15,j/25,1))
+            circle(190-m,90)
+            left(18)
+
+'''
+#################
+#colored hexagon#
+#################
+'''
+import turtle
+from turtle import*
+
+turtle.title("Qobiljon")
+speed(30)
+bgcolor("black")
+r,g,b=255,0,0
+
+for i in range(255*2):
+    colormode(255)
+    if i<255//3:
+        g+=3
+    elif i<255*2//3:
+        r-=3
+    elif i<255:
+        b+=3
+    elif i<255*4//3:
+        g-=3
+    elif i<255*5//3:
+        r+=3
+    else:
+        b-=3
+    fd(50+i)
+    rt(60)
+    pencolor(r,g,b)
+'''
+###################
+#colored square####
+###################
+'''
+import turtle
+from turtle import*
+
+turtle.title("Qobiljon")
+speed(30)
+bgcolor("black")
+r,g,b=255,0,0
+
+for i in range(255*2):
+    colormode(255)
+    if i<255//3:
+        g+=3
+    elif i<255*2//3:
+        r-=3
+    elif i<255:
+        b+=3
+    elif i<255*4//3:
+        g-=3
+    elif i<255*5//3:
+        r+=3
+    else:
+        b-=3
+    fd(50+i)
+    rt(130)
+    pencolor(r,g,b)
+'''
+################
+#covid-19 😅###
+###############
+'''
+import turtle
+from turtle import*
+
+turtle.title("Qobiljon")
+speed(30)
+bgcolor("black")
+r,g,b=255,0,0
+
+for i in range(255*2):
+    colormode(255)
+    if i<255//3:
+        g+=3
+    elif i<255*2//3:
+        r-=3
+    elif i<255:
+        b+=3
+    elif i<255*4//3:
+        g-=3
+    elif i<255*5//3:
+        r+=3
+    else:
+        b-=3
+    fd(50+i)
+    rt(170)
+    pencolor(r,g,b)
+'''
+###############
+#   Virus     #
+###############
+'''
+import turtle
+from turtle import*
+
+
+speed(30)
+bgcolor("black")
+r,g,b=255,0,0
+
+for i in range(255*2):
+    colormode(255)
+    if i<255//3:
+        g+=3
+    elif i<255*2//3:
+        r-=3
+    elif i<255:
+        b+=3
+    elif i<255*4//3:
+        g-=3
+    elif i<255*5//3:
+        r+=3
+    else:
+        b-=3
+    fd(50+i)
+    rt(150)
+    pencolor(r,g,b)
+'''
+#######################
+#curved quadrilateral##
+#######################
+'''
+
+import turtle
+from turtle import*
+speed(30)
+bgcolor("black")
+r,g,b=255,0,0
+
+for i in range(255*2):
+    colormode(255)
+    if i<255//3:
+        g+=3
+    elif i<255*2//3:
+        r-=3
+    elif i<255:
+        b+=3
+    elif i<255*4//3:
+        g-=3
+    elif i<255*5//3:
+        r+=3
+    else:
+        b-=3
+    fd(50+i)
+    rt(100)
+    pencolor(r,g,b)
+'''
+#################
+#colored stars###
+#################
+'''
+import turtle
+from turtle import*
+
+turtle.title("Qobiljon")
+speed(30)
+bgcolor("black")
+r,g,b=255,0,0
+
+for i in range(255*2):
+    colormode(255)
+    if i<255//3:
+        g+=3
+    elif i<255*2//3:
+        r-=3
+    elif i<255:
+        b+=3
+    elif i<255*4//3:
+        g-=3
+    elif i<255*5//3:
+        r+=3
+    else:
+        b-=3
+    fd(50+i)
+    rt(160)
+    pencolor(r,g,b)
+'''
+##########################
+#colored stars############
+##########################
+'''
+import turtle
+from turtle import*
+
+turtle.title("Qobiljon")
+speed(30)
+bgcolor("black")
+r,g,b=255,0,0
+
+for i in range(255*2):
+    colormode(255)
+    if i<255//3:
+        g+=3
+    elif i<255*2//3:
+        r-=3
+    elif i<255:
+        b+=3
+    elif i<255*4//3:
+        g-=3
+    elif i<255*5//3:
+        r+=3
+    else:
+        b-=3
+    fd(50+i)
+    rt(203)
+    pencolor(r,g,b)
+'''
+########################
+#colored cones and leaf#
+########################
+'''
+from turtle import*
+bgcolor("black")
+pensize(5)
+r,g,b=255,0,0
+speed(20)
+for i in range(255*2):
+    colormode(255)
+    if i<255//3:
+        g+=3
+    elif i<255*2//3:
+        r-=3
+    elif i<255:
+        b+=3
+    elif i<255*4//3:
+        g-=3
+    elif i<255*5//3:
+        r+=3
+    else:
+        b-=3
+    pencolor(r,g,b)
+    circle(180-i)
+    lt(90)
+'''
+#################
+#colored circles#
+#################
+'''
+from turtle import*
+bgcolor("black")
+pensize(2)
+r,g,b=255,0,0
+speed(20)
+for i in range(255*2):
+    colormode(255)
+    if i<255//3:
+        g+=3
+    elif i<255*2//3:
+        r-=3
+    elif i<255:
+        b+=3
+    elif i<255*4//3:
+        g-=3
+    elif i<255*5//3:
+        r+=3
+    else:
+        b-=3
+    pencolor(r,g,b)
+    circle(180)
+    lt(18)
+'''
+########################
+#variable color pattern#
+########################
+'''
+from turtle import*
+bgcolor("black")
+pensize(2)
+r,g,b=255,0,0
+speed(20)
+for i in range(255*2):
+    colormode(255)
+    if i<255//3:
+        g+=3
+    elif i<255*2//3:
+        r-=3
+    elif i<255:
+        b+=3
+    elif i<255*4//3:
+        g-=3
+    elif i<255*5//3:
+        r+=3
+    else:
+        b-=3
+    pencolor(r,g,b)
+    circle(180,150)
+    lt(60)
+
+'''
